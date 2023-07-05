ad2d9ba7e06d02c4840ca7e9a39ba08bb262db1e
Sim-km <73809814+tlarbals824@users.noreply.github.com>
2023-07-05 16:50:02 +0900

    [Silver II] Title: 마인크래프트, Time: 152 ms, Memory: 3428 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.cc
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.cc
@@ -7,23 +7,23 @@ int n,m; // 세로 n, 가로 m
 int b; // 인벤토리 블록 수
 
 int totalHeight = 0;
-int avgHeight = 0;
 int needBlockCnt = 0;
 
-int field[1000][1000];
+int field[600][600];
 
 int resultTime = 10e8;
 int resultHeight = 0;
+
+int plusCnt = 0;
+int minusCnt = 0;
 
 void input(){
     cin>>n>>m>>b;
     for(int i = 0;i<n;i++){
         for(int j =0; j< m ;j++){
             cin >> field[i][j];
-            totalHeight += field[i][j];
         }
     }
-    avgHeight = totalHeight / (n*m);
 }
 
 void output(){
@@ -32,36 +32,35 @@ void output(){
 
 bool checkNeedBlockCnt(int height){
     int needBlockCnt = 0;
+    plusCnt=minusCnt=0;
     for(int i = 0 ; i < n ; i++){
         for(int j = 0; j< m ; j++){
-            needBlockCnt += (field[i][j]-height);
+            int tmp = field[i][j]-height;
+            needBlockCnt += tmp;
+            if(tmp > 0){
+                plusCnt+=tmp;
+            }else{
+                minusCnt-=tmp;
+            }
         }
     }
     return (b + needBlockCnt) >= 0;
 }
 
 void timeCheck(int height){
-    int tmpTime = 0;
-    for(int i = 0; i < n ; i++){
-        for(int j = 0 ; j < m ; j++){
-            int fieldHeight = field[i][j];
-            if(fieldHeight > height){
-                tmpTime+=(2*(fieldHeight-height));
-            }else if(fieldHeight < height){
-                tmpTime+=(height-fieldHeight);
-            }
-        }
-    }
-    if(tmpTime < resultTime){
+    int tmpTime = 2*plusCnt+minusCnt;
+    if(tmpTime <= resultTime){
         resultTime = tmpTime;
         resultHeight = height;
     }
 }
 
 void bruteForce(){
-    for(int i = 256; i >= 0 ; i--){
+    for(int i = 0; i <= 256 ; i++){
         if(checkNeedBlockCnt(i)){
             timeCheck(i);
+        }else{
+            break;
         }
     }
 }
