55517b2e25f1b5a18b83f0b7ddc82ead676e5a12
Sim-km <73809814+tlarbals824@users.noreply.github.com>
2023-07-10 21:14:27 +0900

    [Silver I] Title: 카잉 달력, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+int t;
+int n,m,x,y;
+// n*a+x = m*b+y
+
+int cal(){
+    for(int i = 0 ; i <= n ;i++){
+        int tmp = m*i+y;
+        if((tmp-x)%n==0){
+            return tmp;
+        }
+    }
+    return -1;
+}
+
+void input(){
+    cin>>t;
+    for(int i = 0 ; i<t;i++){
+        cin>>n>>m>>x>>y;
+        cout<<cal()<<'\n';    
+    }
+}
+
+void start(){
+    input();
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    start();
+}
