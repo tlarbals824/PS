e55bd4e8351720233b1e7bf054333ba7ea87170b
Sim-km <73809814+tlarbals824@users.noreply.github.com>
2023-07-11 20:56:43 +0900

    [Gold IV] Title: 테트로미노, Time: 176 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.cc
+++ 백준/Gold/14500. 테트로미노/테트로미노.cc
@@ -7,10 +7,8 @@ int n, m;
 int paper[1000][1000][2] = {0};
 int result = 0;
 
-int dirX[4] = {-1, 0, 1, 0};
-int dirY[4] = {0, -1, 0, 1};
-// int tmpY = 0;
-// int tmpX = 0;
+int dirX[3] = {0,1, 0};
+int dirY[3] = {-1,0, 1};
 
 void input() {
   cin >> n >> m;
@@ -23,11 +21,11 @@ void input() {
 
 void output() { cout << result << '\n'; }
 
-void dfs(int cnt, int y, int x, int previousY, int previousX, int sum) {
+void dfs(int cnt, int y, int x, int previousY, int previousX, int sum) { 
   if (cnt == 3) {
     result = max(result, sum);
   } else {
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 3; i++) {
       int tmpY = y + dirY[i];
       int tmpX = x + dirX[i];
       if (tmpY < 0 || tmpY >= n) continue;
@@ -36,9 +34,10 @@ void dfs(int cnt, int y, int x, int previousY, int previousX, int sum) {
       paper[tmpY][tmpX][1] = 1;
       dfs(cnt+1, tmpY, tmpX, y, x, sum + paper[tmpY][tmpX][0]);
       paper[tmpY][tmpX][1] = 0;
-
-      tmpY = previousY + dirY[i];
-      tmpX = previousX + dirX[i];
+    }
+    for(int i=0;i<3;i++){
+      int tmpY = previousY + dirY[i];
+      int tmpX = previousX + dirX[i];
       if (tmpY < 0 || tmpY >= n) continue;
       if (tmpX < 0 || tmpX >= m) continue;
       if (paper[tmpY][tmpX][1]) continue;
