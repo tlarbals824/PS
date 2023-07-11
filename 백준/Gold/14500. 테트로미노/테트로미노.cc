6df966e5b23b66b0383243b6c5f5ed9d38917ba8
Sim-km <73809814+tlarbals824@users.noreply.github.com>
2023-07-11 17:46:24 +0900

    [Gold IV] Title: 테트로미노, Time: 288 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.cc
@@ -0,0 +1,73 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int paper[1000][1000][2] = {0};
+int result = 0;
+
+int dirX[4] = {-1, 0, 1, 0};
+int dirY[4] = {0, -1, 0, 1};
+// int tmpY = 0;
+// int tmpX = 0;
+
+void input() {
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> paper[i][j][0];
+    }
+  }
+}
+
+void output() { cout << result << '\n'; }
+
+void dfs(int cnt, int y, int x, int previousY, int previousX, int sum) {
+  if (cnt == 3) {
+    result = max(result, sum);
+  } else {
+    for (int i = 0; i < 4; i++) {
+      int tmpY = y + dirY[i];
+      int tmpX = x + dirX[i];
+      if (tmpY < 0 || tmpY >= n) continue;
+      if (tmpX < 0 || tmpX >= m) continue;
+      if (paper[tmpY][tmpX][1]) continue;
+      paper[tmpY][tmpX][1] = 1;
+      dfs(cnt+1, tmpY, tmpX, y, x, sum + paper[tmpY][tmpX][0]);
+      paper[tmpY][tmpX][1] = 0;
+
+      tmpY = previousY + dirY[i];
+      tmpX = previousX + dirX[i];
+      if (tmpY < 0 || tmpY >= n) continue;
+      if (tmpX < 0 || tmpX >= m) continue;
+      if (paper[tmpY][tmpX][1]) continue;
+      paper[tmpY][tmpX][1] = 1;
+      dfs(cnt+1, tmpY, tmpX, previousY, previousX, sum + paper[tmpY][tmpX][0]);
+      paper[tmpY][tmpX][1] = 0;
+    }
+  }
+}
+
+void bruteForce() {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      paper[i][j][1] = 1;
+      dfs(0, i, j, i, j, paper[i][j][0]);
+      paper[i][j][1] = 0;
+    }
+  }
+}
+
+void start() {
+  input();
+  bruteForce();
+  output();
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  start();
+}
