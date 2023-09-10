0e4c49dd0fe2ab01b7b87d2fe199d1cec73860e7
Sim-km <73809814+tlarbals824@users.noreply.github.com>
2023-09-10 17:14:22 +0900

    [level 2] Title: 전화번호 목록, Time: 123.64 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,32 +1,13 @@
 #include <string>
 #include <vector>
 #include<algorithm>
-#include<iostream>
 
 using namespace std;
 
-vector<string> tmpString[30];
-int maxLength = 0;
-
-bool compare(string first, string second){
-    return first.size() < second.size();
-}
-
 bool solution(vector<string> phone_book) {
-    sort(phone_book.begin(), phone_book.end(), compare); // nlogn
-    for(int i=0;i<phone_book.size();i++){
-        tmpString[phone_book[i].size()].push_back(phone_book[i]); // n
-        maxLength=max(maxLength,(int)phone_book[i].length());
-    }
-    for(int i=0;i<phone_book.size();i++){
-        for(int j=phone_book[i].size()+1;j<=maxLength;j++){
-            for(int k = 0; k<tmpString[j].size();k++){
-                for(int x = 0;x<tmpString[j][k].size();x++){
-                    if(tmpString[j][k][x]!=phone_book[i][x]) break;
-                    if(x+1==phone_book[i].size()) return false;
-                }
-            }
-        }
+    sort(phone_book.begin(), phone_book.end());
+    for(int i =0;i<phone_book.size()-1;i++){
+        if(phone_book[i]==phone_book[i+1].substr(0,phone_book[i].size())) return false;
     }
     return true;
 }
