09043ab46b0494c6a1b6b280f20468739a95a8ea
Sim-km <73809814+tlarbals824@users.noreply.github.com>
2023-10-05 19:20:26 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n,m;
+
+vector<int> ary;
+vector<vector<int>> tmp;
+
+void input(){
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        int num;
+        cin>>num;
+        ary.push_back(num);
+    }
+    sort(ary.begin(),ary.end());
+}
+
+bool check(vector<int> tmp, int idx){
+    for(int i=0;i<tmp.size();i++){
+        if(tmp[i]==idx) return false;
+    }
+    return true;
+}
+
+void recursive(vector<int> tmp, int cnt, int idx){
+    if(cnt>m) return;
+    if(cnt==m) {
+        for(int i=0;i<tmp.size();i++){
+            cout<<tmp[i]<<' ';
+        }
+        cout<<'\n';
+    }else if(idx==n){
+        return;
+    }else{
+        tmp.push_back(ary[idx]);
+        recursive(tmp, cnt+1, idx);
+        tmp.pop_back();
+        recursive(tmp, cnt, idx+1);
+    }
+}
+
+void start(){
+    input();
+    recursive(vector<int>(),0,0);
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    start();
+}
