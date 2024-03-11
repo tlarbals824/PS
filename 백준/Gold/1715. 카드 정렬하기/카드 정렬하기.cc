6854a3d3771a41e55838db2b292832552e8136f3
Sim-km <simkm0824@naver.com>
2024-03-12 00:54:46 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 28 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.cc
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int n;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int result=0;
+
+void input(){
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int num;
+        cin>>num;
+        pq.push(num);
+    }
+}
+
+void output(){
+    cout<<result<<'\n';
+}
+
+void cal(){
+    int sum =0;
+    while(!pq.empty()){
+        int top = pq.top();
+        pq.pop();
+
+        if(sum==0){
+            sum = top;
+            continue;
+        }
+
+        sum+=top;
+        result+=sum;
+        pq.push(sum);
+        sum=0;
+    }
+    
+}
+
+void start(){
+    input();
+    cal();
+    output();
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    start();
+}
