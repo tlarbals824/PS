8ffbd088397d313fcca8ae10d49cf13764d1c731
Sim-km <simkm0824@naver.com>
2024-03-23 00:18:46 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 5.17 ms, Memory: 24.7 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
@@ -5,16 +5,15 @@
 using namespace std;
 
 long long calculate(vector<int> sequence, int parse){
-    long long check[500100][2]={0};
+    long long check[500100]={0};
     
     long long maxNum=-1;
     
     for(int i=1;i<=sequence.size();i++){
-        check[i][0]=sequence[i-1]*parse;
-        check[i][1]=max(check[i-1][0], check[i-1][1])+sequence[i-1]*parse;
+        check[i]=max((long long)sequence[i-2]*-1*parse, check[i-1])+sequence[i-1]*parse;
         parse*=-1;
         
-        maxNum=max(check[i][0],max(check[i][1],maxNum));
+        maxNum=max(check[i],maxNum);
     }
     
     return maxNum;
