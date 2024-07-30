c2be3d1e6373b765fbe7be2c4b3386a8fe6f096d
Sim-km <simkm0824@naver.com>
2024-07-30 17:10:21 +0900

    [Silver V] Title: 돌 게임 2, Time: 132 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println((n & 1) == 0 ? "SK" : "CY");
+    }
+
+}
